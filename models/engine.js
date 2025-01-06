3b55e7a934071d961474e73288ffa3d5c50a6225
Chamika Deshan Jayasingha <chamikadeshan97@gmail.com>
2025-01-07 01:26:45 +0530

    sdfsdf

--- models/engine.js
+++ models/engine.js
@@ -1,20 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const engineSchema = new mongoose.Schema({
-  class: { type: String, required: true },
-  subClass:{ type: String, required: true , unique: true},
-   num:{ type: String, required: true },
-   year:{ type: String, required: false },
-   country:{ type: String, required: false },
+const engineSchema = new mongoose.Schema(
+  {
+    class: { type: String, required: true },
+    subClass: { type: String, required: true, unique: true },
+    num: { type: String, required: false },
+    year: { type: String, required: false },
+    country: { type: String, required: false },
+    company: { type: String, required: false },
+    axleStructure: { type: String, required: false },
+    powerEngine: { type: String, required: false },
+    powerHp: { type: String, required: false },
+    specs: { type: String, required: false },
 
-   company:{ type: String, required: true },
-   axleStructure:{ type: String, required: true },
-   powerEngine:{ type: String, required: false },
-   powerHp:{ type: String, required: false },
-   specs:{ type: String, required: false },
-   
-   condition:{ type: String, required: true },
-   
-}, { timestamps: true });
+    condition: { type: String, required: true },
+    shed: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Engine', engineSchema);
+module.exports = mongoose.model("Engine", engineSchema);
--- routes/engineRoutes.js
+++ routes/engineRoutes.js
@@ -1,33 +1,52 @@
-const express = require('express');
-const Engine = require('../models/engine');
-const protectRoute = require('../authMiddleware');
+const express = require("express");
+const Engine = require("../models/engine");
+const protectRoute = require("../authMiddleware");
 
 const router = express.Router();
 
 // Get All Engines
-router.get('/' ,async (req, res) => {  
+router.get("/", async (req, res) => {
   try {
     const engines = await Engine.find();
 
     res.status(200).json(engines);
-  } catch (err) {    
-    res.status(500).json({ error: 'Failed to fetch Engines' });
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch Engines" });
   }
 });
 
-router.post('/',async (req, res) => {  
-  const { type, shed, unitType,utilization,condition,remarks } = req.body;
+router.post("/", async (req, res) => {
+  console.log(req.body);
 
-  console.log(type, shed, unitType,utilization,condition,remarks);
-  
+  const { type, shed, unitType, utilization, condition, remarks } = req.body;
+
+  console.log(type, shed, unitType, utilization, condition, remarks);
+
   try {
-    const newEngine = new Engine({ type, shed, unitType,utilization,condition,remarks });
+    const newEngine = new Engine(req.body);
     await newEngine.save();
     res.status(201).json(newEngine);
   } catch (err) {
-    
-    res.status(500).json({ error: 'Failed to create Engine' });
+    console.log(err);
+
+    res.status(500).json({ error: "Failed to create Engine" });
   }
 });
-
+router.get("/engineBySubClass", async (req, res) => {
+  console.log('engineBySubClass');
+  const { subClass } = req.query; // Get subClass from query params
+  console.log(subClass);
+  
+  try {
+    const engines = await Engine.find({ subClass });
+    if (engines.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "No engines found for this subClass" });
+    }
+    res.json(engines);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch Engine" });
+  }
+});
 module.exports = router;
