2b1289deb572210d7ec1ca7ac4587406c8d08ebe
Chamika Deshan Jayasingha <chamikadeshan97@gmail.com>
2025-01-31 10:18:41 +0530

    s

--- routes/tripCardRoutes.js
+++ routes/tripCardRoutes.js
@@ -4,7 +4,7 @@ const protectRoute = require("../authMiddleware");
 
 const router = express.Router();
 
-router.get("/",protectRoute, async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const trips = await TripCards.find();
 
@@ -14,19 +14,17 @@ router.get("/",protectRoute, async (req, res) => {
   }
 });
 
-router.post("/",protectRoute, async (req, res) => {
- 
+router.post("/", async (req, res) => {
   try {
     const newTripCard = new TripCards(req.body);
     await newTripCard.save();
     res.status(201).json(newTripCard);
-    
-  } catch (err) {    
+  } catch (err) {
     res.status(500).json({ error: "Failed to create new Trip Card" });
   }
 });
-router.get("/tripsByTrainNumber",protectRoute, async (req, res) => {
-  const { trainNumber } =req.body.trainNumber;
+router.get("/tripsByTrainNumber", async (req, res) => {
+  const { trainNumber } = req.body.trainNumber;
   try {
     const trips = await TripCards.find(trainNumber);
     if (trips.length === 0) {
