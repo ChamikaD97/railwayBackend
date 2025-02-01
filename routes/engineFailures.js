adc02172f2120b1f9be285098f5933e7e59003fd
Chamika Deshan Jayasingha <chamikadeshan97@gmail.com>
2025-02-01 17:06:16 +0530

    asd

--- routes/engineFailures.js
+++ routes/engineFailures.js
@@ -5,9 +5,16 @@ const protectRoute = require("../authMiddleware");
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  const { date, failure, drivcerComNum, engine,trainNumber, comments, status, risk } =
-    req.body;
-
+  const {
+    date,
+    failure,
+    drivcerComNum,
+    engine,
+    trainNumber,
+    comments,
+    status,
+    risk,
+  } = req.body;
 
   try {
     const newengineFailures = new engineFailures({
@@ -23,13 +30,11 @@ router.post("/", async (req, res) => {
     await newengineFailures.save();
     res.status(201).json(newengineFailures);
   } catch (err) {
-  
-
     res.status(500).json({ error: "Failed to create EngineFailures" });
   }
 });
 
-router.get("/",async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const failures = await engineFailures.find().sort({ updatedAt: -1 }); // Use 1 for ascending, -1 for descending
 
@@ -38,24 +43,43 @@ router.get("/",async (req, res) => {
     res.status(500).json({ error: "Failed to fetch Engine Failures" });
   }
 });
+router.get("/engineFailuresByDrivcerComNum", async (req, res) => {
+  const data = {
+    drivcerComNum: req.query.drivcerComNum,
+  };
 
+  try {
+    const failures = await engineFailures.find(data);
 
+    res.status(200).json(failures);
+  } catch (err) {
+    res.status(500).json({ error: "No Failures To  User" });
+  }
+});
+router.get("/engineFailureByEngine", async (req, res) => {
+  const data = {
+    engine: req.query.engine,
+  };
+
+  try {
+    const failures = await engineFailures.find(data);
+    res.status(200).json(failures);
+  } catch (err) {
+    res.status(500).json({ error: "No Failures To This Engine" });
+  }
+});
 router.get("/engineFailureById", async (req, res) => {
   try {
     const failures = await engineFailures.findById(req.query.id);
 
-
     res.status(200).json(failures);
   } catch (err) {
-    
-
     res.status(500).json({ error: "Failed to fetch Engine Failures" });
   }
 });
 
 router.put("/updateById", async (req, res) => {
   const { id, ...updateData } = req.body;
-
 
   try {
     const updatedFailure = await engineFailures.findByIdAndUpdate(
@@ -70,7 +94,6 @@ router.put("/updateById", async (req, res) => {
 
     res.status(201).json(updatedFailure);
   } catch (err) {
-
     res.status(500).json({ error: "Failed to update Engine Failure" });
   }
 });
