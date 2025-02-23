f14a5d20efa96e717edad58dfdd734f27f168dcb
Chamika Deshan Jayasingha <chamikadeshan97@gmail.com>
2025-02-23 22:19:20 +0530

    werwer

--- models/inspections.js
+++ models/inspections.js
@@ -21,11 +21,11 @@ const inspectionSchema = new mongoose.Schema(
 
     isDoubleSet: { type: Boolean, default: false }, //
 
-    doubleSetLink:{ type: String, required: false , default:''},
-
+    doubleSetLink: { type: String, required: false, default: "" },
+    falseCount: { type: String, required: false,default: "0" },
+    trueCount: { type: String, required: false , default: "12" },
   },
   { timestamps: true }
 );
 
 module.exports = mongoose.model("inspection", inspectionSchema);
-
